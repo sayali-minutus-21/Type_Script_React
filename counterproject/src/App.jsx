e556ac8cc5c16efc5b022a7886fd7ed1171b4b03
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-02-07 17:34:31 +0530

    useState hook in react

--- counterproject/src/App.jsx
+++ counterproject/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'//hook(bable)
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {

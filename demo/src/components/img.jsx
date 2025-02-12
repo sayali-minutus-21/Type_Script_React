804a237458e7b10a275ad3de5374a51b65b5efc2
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-02-12 11:21:02 +0530

    using props

---
+++ demo/src/components/img.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Img(props){
+    console.log(props);
+    return (
+        <div>
+            <img src="marguerite-729510_1280.jpg" alt="marguerite" />
+        </div>
+    );
+}
+
+export default Img;
